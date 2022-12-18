f679234432d26f0d5447964713ea83bfb3b84b52
Visinigiri Aditya <adityakumarverified@gmail.com>
2022-12-18 21:10:27 +0530

    first commit

---
+++ src/components/Table.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+interface ItableProps {
+  tableNumber: number;
+  status: string;
+}
+
+const Table = ({ tableNumber, status }: ItableProps) => {
+  return (
+    <div
+      className={`${
+        status === "available" ? "bg-green-900" : "bg-gray-700"
+      } p-4 text-center`}
+    >
+      <h3 className="text-3xl">{tableNumber}</h3>
+      <span>{status}</span>
+    </div>
+  );
+};
+
+export default Table;
